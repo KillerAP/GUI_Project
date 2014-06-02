436e53eca2193ad23c5422f1716af3810ec0ae00
Seyon <seyon.vasantharajan@gmail.com>
2014-06-02 17:27:32 -0400

    modified sSF.m
    Added help function description

--- sSF.m
+++ sSF.m
@@ -1,8 +1,15 @@
 function [ SF_x, SF_var] = sSF( r_it, r_M, return_range, mu, mu_M)
-%SSF Summary of this function goes here
-%   Detailed explanation goes here
+%% SINGLE FACTOR MEAN VARIANCE OPTIMIZATION VIA QUADPROG
+% Input the historical return data for assets, return data for the market, range of desired returns
+% the mean vector for the assets and the mean return of the market. The function will return
+% optimal portfolio weights in SF_x, and the corresponding variance 
+% function value in SF_var.
+%
+% [optimal weights, corresponding optimal objective function] 
+%   = sMVO(asset return data, market return data,range of desired returns, expected return vector for assets, 
+%          expected return of the market)
 
-el_M=sum((r_M(:,1) - mu_M(1)).^2)/T; % Factor variance
+del_M=sum((r_M(:,1) - mu_M(1)).^2)/T; % Factor variance
 beta=(sum(r_it(:,1:end).*repmat(r_M(:,1),1,n))/T-mean(r_it(:,1:end)*...
     mean(r_M(:,1)))/del_M); 
 alpha = mu(1:end)-beta*mu_M;
